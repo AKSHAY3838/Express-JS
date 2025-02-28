bea4c7c05e381d4d9443443b493073a2f95c88e2
ak3838 <akshay8971573838@gmail.com>
2025-02-28 14:30:53 +0530

    accrssing object all the objects

--- sample.js
+++ sample.js
@@ -34,19 +34,38 @@ const express =require("express")
 const app=express()
 const port =3838
 // single user details in object 
-app.get("/",(req,res) => {
-    const user =users[0]
-    res.send(`<center><h1>1st user details</h1>
-                <p><strong>Name:</strong> ${user.Fname}</p>
+// app.get("/",(req,res) => {
+//     const user =users[0]
+//     res.send(`<center><h1>1st user details</h1>
+//                 <p><strong>Name:</strong> ${user.Fname}</p>
+//                 <p><strong>Age:</strong> ${user.age}</p>
+//                 <p><strong>Date of Birth:</strong> ${user.DOB.date} ${user.DOB.month} ${user.DOB.year}</p>
+//                 <p><strong>Address:</strong> ${user.adds}</p>\
+//             </center>
+//     `
+//     ) 
+// })
+
+// // all user details
+// app.get("/", (req, res) => {
+//     let userDetailsHTML = "<center><h1>All User Details</h1>";
+
+    
+//     users.forEach(user => {
+        userDetailsHTML += `
+            <div style="border: 2px solid black; padding: 10px; margin: 10px; width: 300px; text-align: left; background-color: lightblue; border-raius:2px;">
+                <p><strong>Name:</strong> ${user.user1}</p>
                 <p><strong>Age:</strong> ${user.age}</p>
                 <p><strong>Date of Birth:</strong> ${user.DOB.date} ${user.DOB.month} ${user.DOB.year}</p>
-                <p><strong>Address:</strong> ${user.adds}</p>\
-            </center>
-    `
-    )
-    
-})
+                <p><strong>Address:</strong> ${user.adds}</p>
+            </div>
+        `;
+    });
 
+    userDetailsHTML += "</center>";
+    res.send(userDetailsHTML);
+
+})
 
 
 app.listen(port,()=>{
