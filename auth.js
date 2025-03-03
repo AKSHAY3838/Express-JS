a49b7fc5a6a70da46b5f940326ebef2859e55e2b
ak3838 <akshay8971573838@gmail.com>
2025-03-04 00:00:22 +0530

    some changes

--- auth.js
+++ auth.js
@@ -2,8 +2,8 @@ const express = require ("express")
 const app =express();
 const port=4141
 app.get("/",function (req,res) {
-    const username=req.query.username;
-    const password=req.query.password;
+    const username = req.get("username");
+    const password = req.get("password");
     if (username!="user" && password!="pass") {
         res.status(400).json({
             "msg": " invalid inputs try again"
