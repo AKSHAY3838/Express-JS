e358f81ab8446785f3a138fad181c7de03e6f716
ak3838 <akshay8971573838@gmail.com>
2025-03-05 22:42:15 +0530

    authentication by using Middlewares

--- auth.js
+++ auth.js
@@ -1,22 +1,49 @@
 const express = require ("express") 
 const app =express();
 const port=4141
-app.get("/",function (req,res) {
-    const username = req.get("username");
-    const password = req.get("password");
-    if (username!="user" && password!="pass") {
-        res.status(400).json({
-            "msg": " invalid inputs try again"
-        })
-    }
-    if (username==="user" && password === "pass") {
-        res.status(200).json({
-            "msg": " login success"
-        })
-    }
-})
+// app.get("/",function (req,res) {
+//     const username = req.header("username");
+//     const password = req.header("password");
+//     if (username!=="user" || password!=="pass") {
+//        return res.status(400).json({
+//             "msg": " invalid inputs try again"
+//         })
+//     }
+//     if (username==="user" && password === "pass") {
+//         res.status(200).json({
+//             "msg": " login success"
+//         })
+//     }
+// })
 
-app.listen(port, ()=> {
-    console.log("working fine");
+// app.listen(port, ()=> {
+//     console.log("working fine");
+    
+// })
+
+
+// by using middlewares 
+
+
+function usernamevalidation(req,res,next){
+    const uname=req.header("uname")
+    if (uname!=="user") {
+        return res.status(401).send("invcalid user name ");
+    }
+    next();
+}
+function passwordvalidation(req,res,next){
+    const upass=req.header("upass")
+    if (upass!=="password") {
+        return res.status(401).send("invalid user password ");
+    }
+    next();
+}
+
+app.get("/",usernamevalidation,passwordvalidation,function (req,res) {
+    res.send(" login successfully ")
+})
+app.listen(port,()=>{
+    console.log(`server is working fine on port number ${port}`);
     
 })
